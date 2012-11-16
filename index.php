2d6ad8494714553e2de9f21f556c0fd85d9a1031
Murilo <murilo@murilo-Inspiron-1545.(none)>
2012-11-16 17:10:04 -0200

    Estrutura básica para criar o jogo

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Erro Game</title>
+        <script src="js/crafty.js" type="text/javascript"></script>
+        <script src="js/game.js" type="text/javascript"></script>
+        <style></style>
+    </head>
+    <body>
+        
+    </body>
+</html>
