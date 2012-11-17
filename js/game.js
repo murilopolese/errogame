9a99993c2e6691e680a2437c33ab9e64f536f989
Murilo Polese <murilopolese@gmail.com>
2012-11-17 00:02:47 -0200

    Inimigos andando pra esquerda e aumentando a cada round. Player atirando e eliminando inimigos \m/

--- js/game.js
+++ js/game.js
@@ -1,10 +1,107 @@
 $(document).ready(function() {
+    var round = 1;
     Crafty.init(800, 576);
     Crafty.canvas.init();
     Crafty.sprite(32, "img/game_32.png", {
         player: [0,4],
         enemy1: [6,0],
         terrain: [0,8]
+    });
+    Crafty.c('Hero', {
+        init:  function() {
+            this.requires("SpriteAnimation, Collision")
+            .animate("walk_left", [[0,5], [1,5], [2,5]])
+            .animate("walk_right", [[0,6], [1,6], [2,6]])
+            .animate("walk_up", [[0,7], [1,7], [2,7]])
+            .animate("walk_down", [[0,4], [1,4], [2,4]])
+            .bind("NewDirection",
+                function (direction) {
+                    if (direction.x < 0) {
+                        if (!this.isPlaying("walk_left"))
+                            this.stop().animate("walk_left", 10, -1);
+                    }
+                    if (direction.x > 0) {
+                        if (!this.isPlaying("walk_right"))
+                            this.stop().animate("walk_right", 10, -1);
+                    }
+                    if (direction.y < 0) {
+                        if (!this.isPlaying("walk_up"))
+                            this.stop().animate("walk_up", 10, -1);
+                    }
+                    if (direction.y > 0) {
+                        if (!this.isPlaying("walk_down"))
+                            this.stop().animate("walk_down", 10, -1);
+                    }
+                    if(!direction.x && !direction.y) {
+                        this.stop();
+                    }
+                })
+            .bind('Moved', function(from) {
+                if(this.hit('solid')){
+                    this.attr({
+                        x: from.x, 
+                        y:from.y
+                    });
+                }
+            });
+            return this;
+        }
+    });
+    Crafty.c('Enemy', {
+        init: function() {
+            this.requires("SpriteAnimation, Collision")
+            .animate("walk_left", [[6,1], [7,1], [8,1]])
+            .animate("walk_left", 3, -1)
+//            .animate("walk_right", [[6,2], [7,2], [8,2]])
+//            .animate("walk_up", [[6,3], [7,3], [8,3]])
+//            .animate("walk_down", [[6,0], [7,0], [8,0]])
+            .bind("NewDirection",
+                function (direction) {
+                    if (direction.x < 0) {
+                        if (!this.isPlaying("walk_left"))
+                            this.stop().animate("walk_left", 10, -1);
+                    }
+                    if (direction.x > 0) {
+                        if (!this.isPlaying("walk_right"))
+                            this.stop().animate("walk_right", 10, -1);
+                    }
+                    if (direction.y < 0) {
+                        if (!this.isPlaying("walk_up"))
+                            this.stop().animate("walk_up", 10, -1);
+                    }
+                    if (direction.y > 0) {
+                        if (!this.isPlaying("walk_down"))
+                            this.stop().animate("walk_down", 10, -1);
+                    }
+                    if(!direction.x && !direction.y) {
+                        this.stop();
+                    }
+                })
+            .bind('Moved', function(from) {
+                if(this.hit('solid')){
+                    this.attr({
+                        x: from.x, 
+                        y:from.y
+                    });
+                }
+            });
+            return this;
+        }
+    })
+    Crafty.c("RightControls", {
+        init: function() {
+            this.requires('Multiway');
+        },
+
+        rightControls: function(speed) {
+            this.multiway(speed, {
+                W: -90, 
+                S: 90, 
+                D: 0, 
+                A: 180
+            })
+            return this;
+        }
     });
     
     Crafty.scene('loading', function() {
@@ -28,128 +125,68 @@ $(document).ready(function() {
     Crafty.scene('loading');
     
     Crafty.scene('main', function() {
-        generateWorld();
-        Crafty.c('Hero', {
-            init:  function() {
-                this.requires("SpriteAnimation, Collision")
-                .animate("walk_left", [[0,5], [1,5], [2,5]])
-                .animate("walk_right", [[0,6], [1,6], [2,6]])
-                .animate("walk_up", [[0,7], [1,7], [2,7]])
-                .animate("walk_down", [[0,4], [1,4], [2,4]])
-                .bind("NewDirection",
-                    function (direction) {
-                        if (direction.x < 0) {
-                            if (!this.isPlaying("walk_left"))
-                                this.stop().animate("walk_left", 10, -1);
-                        }
-                        if (direction.x > 0) {
-                            if (!this.isPlaying("walk_right"))
-                                this.stop().animate("walk_right", 10, -1);
-                        }
-                        if (direction.y < 0) {
-                            if (!this.isPlaying("walk_up"))
-                                this.stop().animate("walk_up", 10, -1);
-                        }
-                        if (direction.y > 0) {
-                            if (!this.isPlaying("walk_down"))
-                                this.stop().animate("walk_down", 10, -1);
-                        }
-                        if(!direction.x && !direction.y) {
-                            this.stop();
-                        }
-                    })
-                .bind('Moved', function(from) {
-                    if(this.hit('solid')){
-                        this.attr({
-                            x: from.x, 
-                            y:from.y
-                        });
-                    }
-                });
-                return this;
-            }
-        });
-        Crafty.c('Enemy', {
-            init: function() {
-                this.requires("SpriteAnimation, Collision")
-                .animate("walk_left", [[6,1], [7,1], [8,1]])
-                .animate("walk_right", [[6,2], [7,2], [8,2]])
-                .animate("walk_up", [[6,3], [7,3], [8,3]])
-                .animate("walk_down", [[6,0], [7,0], [8,0]])
-                .bind("NewDirection",
-                    function (direction) {
-                        if (direction.x < 0) {
-                            if (!this.isPlaying("walk_left"))
-                                this.stop().animate("walk_left", 10, -1);
-                        }
-                        if (direction.x > 0) {
-                            if (!this.isPlaying("walk_right"))
-                                this.stop().animate("walk_right", 10, -1);
-                        }
-                        if (direction.y < 0) {
-                            if (!this.isPlaying("walk_up"))
-                                this.stop().animate("walk_up", 10, -1);
-                        }
-                        if (direction.y > 0) {
-                            if (!this.isPlaying("walk_down"))
-                                this.stop().animate("walk_down", 10, -1);
-                        }
-                        if(!direction.x && !direction.y) {
-                            this.stop();
-                        }
-                    })
-                .bind('Moved', function(from) {
-                    if(this.hit('solid')){
-                        this.attr({
-                            x: from.x, 
-                            y:from.y
-                        });
-                    }
-                });
-                return this;
-            }
+        Crafty.e("2D,Canvas,Color,Mouse").attr({
+            w:Crafty.viewport.width,
+            h:Crafty.viewport.height,
+            x:0,
+            y:0
         })
-        Crafty.c("RightControls", {
-            init: function() {
-                this.requires('Multiway');
-            },
-
-            rightControls: function(speed) {
-                this.multiway(speed, {
-                    UP_ARROW: -90, 
-                    DOWN_ARROW: 90, 
-                    RIGHT_ARROW: 0, 
-                    LEFT_ARROW: 180
-                })
-                return this;
-            }
+        .bind('MouseMove',function(e){
+            
+            })
+        .bind('Click',function(e) {
+            console.log(e);
         });
-        Crafty.c("EnemyControls", {
-            init: function() {
-                this.requires('Multiway');
-            },
-
-            enemyControls: function(speed) {
-                this.multiway(speed, {
-                    W: -90, 
-                    S: 90, 
-                    D: 0, 
-                    A: 180
-                })
-                return this;
-            }
-        });
+        generateWorld();
+        
+        
         player = Crafty.e('2D, Canvas, player, RightControls, Hero, Animate, Collision')
         .attr({
             x: 160, 
             y: 160, 
             z: 1
         })
-        .rightControls(2);
+        .bind('KeyDown', function(e) {
+            if(e.keyCode === Crafty.keys.SPACE) {
+                console.log('Blast!');
+                Crafty.e("2D, DOM, Color, Collision")
+                .color('rgb(0,0,0)')
+                .attr({
+                    x: this._x, 
+                    y: this._y+10, 
+                    w: 4, 
+                    h: 4, 
+                    dX: 10, 
+                    dY: 0
+                })
+                .bind('EnterFrame', function () {
+                    this.x += this.dX;
+                    this.y += this.dY;
+                    if(this._x > Crafty.viewport.width || this._x < 0 || this._y > Crafty.viewport.height || this._y < 0) {
+                        this.destroy();
+                        console.log('removeu o bullet')
+                    }
+                })
+                .onHit('Enemy', function (e) {
+                    console.log(e);
+                    e[0].obj.destroy(); // Destrói o inimigo
+                    this.destroy(); // Destrói a bala
+                })
+            }
+        })
+        .onHit('Enemy', function(e) {
+            clearInterval(interval);
+            Crafty.scene('main');
+        })
+        .rightControls(3);
+        
+        
         generateEnemies(1);
-        var interval = setInterval(function() {
-            generateEnemies(1);
-        }, 20000);
+        interval = setInterval(function() {
+            round += 0.01;
+            generateEnemies(Math.floor(round));
+        }, 1000);
+        
     })
     
     function generateWorld() {
@@ -167,13 +204,22 @@ $(document).ready(function() {
     function generateEnemies(n) {
         if(n > 0) {
             for(i = 0; i < n; i++) {
-                var enemy = Crafty.e('2D, Canvas, enemy1, EnemyControls, Enemy, Animate, Collision')
+                var enemy = Crafty.e('2D, Canvas, enemy1, Enemy, Animate, Collision')
                 .attr({
-                    x: 100, 
-                    y: 32, 
-                    z: 1
+                    x: parseInt(Crafty.viewport.width)-40, 
+                    y: Crafty.math.randomInt(0, Crafty.viewport.height), 
+                    z: 1,
+                    dX: -2,
+                    dY: 0
                 })
-                .enemyControls(1);
+                .bind('EnterFrame', function () {
+                    this.y += this.dY;
+                    this.x += this.dX;
+                    if(this._x > Crafty.viewport.width || this._x < 0 || this._y > Crafty.viewport.height || this._y < 0) {
+                        this.destroy();
+                        console.log('removeu o inimigo')
+                    }
+                });
             }
         }
     }
